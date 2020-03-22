5635b3f54e62f1df6e4b1def9286b903a2cad717
Reinoptland <reinoptland@gmail.com>
2020-03-22 15:07:02 +0100

    Also update my homepage in the list of all homepages on edit

--- src/store/homepages/reducer.js
+++ src/store/homepages/reducer.js
@@ -1,4 +1,5 @@
 import { FETCH_HOMEPAGES_SUCCESS } from "./actions";
+import { HOMEPAGE_UPDATED } from "../user/actions";
 
 const initialState = [];
 
@@ -6,6 +7,15 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_HOMEPAGES_SUCCESS:
       return [...state, ...action.payload];
+    case HOMEPAGE_UPDATED: {
+      return state.map(homepage => {
+        if (homepage.id !== action.payload.id) {
+          return homepage;
+        }
+
+        return { ...action.payload, stories: [...homepage.stories] };
+      });
+    }
     default:
       return state;
   }
